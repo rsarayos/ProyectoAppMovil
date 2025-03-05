822fa7b7edaa4c6f6827eda40f4301cdb98f3830
rsarayos <raul.sauceda@potros.itson.edu.mx>
2025-03-05 11:21:03 -0700

    Ajustes
    correcciones para funcionamiento de las pantallas

--- app/src/main/java/equipo/cinco/planeaciondeeventos/MainActivity.kt
+++ app/src/main/java/equipo/cinco/planeaciondeeventos/MainActivity.kt
@@ -1,6 +1,10 @@
 package equipo.cinco.planeaciondeeventos
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
